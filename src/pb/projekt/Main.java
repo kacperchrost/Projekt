e957478cc11410f6316ebf42ccfd1073c56841e5
Fistoo <fisto321@gmail.com>
2020-06-07 15:41:06 +0200

    test2

--- src/pb/projekt/Main.java
+++ src/pb/projekt/Main.java
@@ -3,6 +3,8 @@ package pb.projekt;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world!");
+
+        System.out.println("Hello world!");
+        System.out.println("Powiedzmy ze dziala");
     }
 }
